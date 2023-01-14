0251193aece1c8f20255afef9b756197ff965ef6
Jully13 <7448778@mail.ru>
2023-01-15 01:29:52 +0300

    feat: writting a function for the task solving

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let str = (Math.abs(n)).toString();
+  let arr =[];
+
+  let j = 0;
+  for (let i=str.length-1; i>=0; i--) {
+    arr[j] = str[i];
+    j++;
+  }
+  return Number(arr.join(''));
 }
