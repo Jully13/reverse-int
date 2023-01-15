d89004a4a9556ce0516b3c05190cb0b2aaaee062
Jully13 <7448778@mail.ru>
2023-01-15 10:21:02 +0300

    feat: implement the method push for the task solving

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ module.exports = function reverse (n) {
   let str = (Math.abs(n)).toString();
   let arr =[];
 
-  let j = 0;
   for (let i=str.length-1; i>=0; i--) {
-    arr[j] = str[i];
-    j++;
+    arr.push(str[i]);
   }
   return Number(arr.join(''));
 }
